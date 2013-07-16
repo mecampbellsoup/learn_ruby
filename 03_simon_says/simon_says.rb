df3e5bfd2532440bf6fa7c9de7162ded1f6edba6
mecampbellsoup <mecampbell25@gmail.com>
2013-07-16 15:58:27 -0400

    simon says methods created and also refactored :)

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 def echo string
   string
 end
@@ -14,3 +16,21 @@ end
 def start_of_word string, num_chars=1
   string[0, num_chars]
 end
+
+def first_word string
+  string[0, string.index(" ")]
+end
+
+def titleize input
+  words_to_lowercase = %w(or and the if but over)
+  input_array = input.split(' ')
+
+  capitalized_word_array = input_array.collect do |input_word|
+    words_to_lowercase.include?(input_word) ? input_word : input_word.capitalize
+  end
+  
+  capitalized_word_array.first.capitalize!
+  capitalized_word_array.join(' ')
+end
+
+titleize "war and peace"
