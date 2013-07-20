62ba685760a0957dd07bebb9350e59bbe4b0bc0a
mecampbellsoup <mecampbell25@gmail.com>
2013-07-20 15:17:17 -0400

    Improved my timer by employing helper method

--- 09_timer/timer.rb
+++ 09_timer/timer.rb
@@ -1,7 +1,9 @@
 require 'pry'
 
 class Timer
+  
   attr_accessor :seconds, :minutes, :hours, :time_string
+  
   def initialize seconds=0
     @seconds = seconds
   end
@@ -10,7 +12,11 @@ class Timer
     (@seconds/3600)>0 ? @hours = (@seconds/3600) : @hours = 0
     ((@seconds/60)-(@hours*60))>0 ? @minutes = ((@seconds/60)-(@hours*60)) : @minutes = 0
     (@seconds - @hours*3600 - @minutes*60)>0 ? @seconds = (@seconds - @hours*3600 - @minutes*60) : 0
-    return "#{"%02d" % @hours}:#{"%02d" % @minutes}:#{"%02d" % @seconds}"
+    "#{padded(hours)}:#{padded(minutes)}:#{padded(seconds)}"
+  end
+
+  def padded number
+    "%02d" % number
   end
 end
 
